a5f77cef17bffc571b638061ff0f4d66afb323fd
Steve LoFurno <slofurno@gmail.com>
2019-03-20 13:41:50 -0400

    mv

--- v201710/customer_feed.go
+++
@@ -1,9 +0,0 @@
-package v201710
-
-type CustomerFeedService struct {
-	Auth
-}
-
-func NewCustomerFeedService(auth *Auth) *CustomerFeedService {
-	return &CustomerFeedService{Auth: *auth}
-}
--- v201710/trial.go
+++
@@ -1,9 +0,0 @@
-package v201710
-
-type TrialService struct {
-	Auth
-}
-
-func NewTrialService(auth *Auth) *TrialService {
-	return &TrialService{Auth: *auth}
-}
