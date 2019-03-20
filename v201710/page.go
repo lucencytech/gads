bcf8dc94352997dc5efb863d53c7e6d8ee93fea1
Steve LoFurno <slofurno@gmail.com>
2019-03-20 13:41:50 -0400

    mv

--- v201710/page.go
+++
@@ -1,8 +0,0 @@
-package v201710
-
-// https://developers.google.com/adwords/api/docs/reference/v201710/AdGroupExtensionSettingService.Page
-// Contains the results from a get call.
-type Page struct {
-	TotalNumEntries int    `xml:"https://adwords.google.com/api/adwords/cm/v201710 totalNumEntries,omitempty"`
-	PageType        string `xml:"https://adwords.google.com/api/adwords/cm/v201710 Page.Type,omitempty"`
-}
--- v201710/text_ad.go
+++
@@ -1,7 +0,0 @@
-package v201710
-
-type Ad struct {
-	AdGroupId int64  `xml:"-"`
-	Id        int64  `xml:"id,omitempty"`
-	Status    string `xml:"-"`
-}
